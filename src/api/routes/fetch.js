e8a0811420a75bfc08540f06008df6de0495ad85
ericbsantana <bitencourteric@hotmail.com>
2021-07-28 17:40:04 -0300

    fix fetch route

--- src/api/routes/fetch.js
+++ src/api/routes/fetch.js
@@ -2,8 +2,8 @@ const FetchController = require("../controller/FetchController");
 const express = require("express");
 const routes = express.Router();
 
-routes.get("/fetch/:country", FetchController.getBestCampaign);
-routes.get("/fetch", FetchController.getCampaigns);
-routes.post("/fetch", FetchController.sendCampaign);
+routes.get("/fetch/:country", FetchController.GetCampaigns);
+routes.get("/fetch", FetchController.GetBestCampaign);
+routes.post("/fetch", FetchController.CreateCampaign);
 
 module.exports = routes;
