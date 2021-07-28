a89c48e1a4a13da38289ea7104daa3b43f45a48e
ericbsantana <bitencourteric@hotmail.com>
2021-07-28 17:21:04 -0300

    add mongoose connection

---
+++ src/api/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const db = require("../database/config");
+const mongoose = require("mongoose");
+
+const userRoute = require("./routes/user");
+const fetchRoute = require("./routes/fetch");
+
+const app = express();
+
+mongoose.connect(db.uri, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+app.use(express.json());
+app.use(userRoute);
+app.use(fetchRoute);
+
+app.listen(3001, () => {
+  console.log("Server is running...");
+});
