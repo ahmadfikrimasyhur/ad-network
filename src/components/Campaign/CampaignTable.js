e141c15cbc6711006c73afc775a72628ef79b1de
ericbsantana <bitencourteric@hotmail.com>
2021-07-31 15:01:26 -0300

    add style to campaign table

--- src/components/Campaign/CampaignTable.js
+++ src/components/Campaign/CampaignTable.js
@@ -16,31 +16,38 @@ const CampaignTable = () => {
 
   useEffect(() => {
     fetchCampaigns();
-  }, [data]);
+  }, []);
 
   return (
-    <table class="table is-fullwidth">
+    <table className="table-fixed shadow-lg bg-gray-50 w-full text-gray-800">
       <thead>
         <tr>
-          <th>Campaign Name</th>
-          <th>Bid</th>
-          <th>Conversion Type</th>
-          <th>Targeting</th>
+          <th className="w-4/6 bg-blue-200 border text-left px-8 py-2">
+            Campaign Name
+          </th>
+          <th className="w-1/8 bg-blue-200 border text-left px-8 py-2">Bid</th>
+          <th className="w-1/8 bg-blue-200 border text-left px-8 py-2">
+            Conversion Type
+          </th>
+          <th className="w-1/8 bg-blue-200 border text-left px-8 py-4">
+            Targeting
+          </th>
         </tr>
       </thead>
       <tbody>
         {data.map((campaign) => (
           <tr>
-            <td>{campaign.name}</td>
-            <td>
+            <td className="border px-8 py-3">{campaign.name}</td>
+            <td className="border px-8 py-3">
               {campaign.bid[0].cost} {campaign.bid[0].currency}
             </td>
-            <td>{campaign.conversion}</td>
-            <td>
+            <td className="border px-8 py-3">{campaign.conversion}</td>
+            <td className="border px-8 py-3">
               {campaign.country ? (
                 <img
                   alt={`${campaign.country} flag`}
                   src={`https://www.countryflags.io/${campaign.country}/flat/24.png`}
+                  className="rounded-md"
                 />
               ) : (
                 `World-wide`
