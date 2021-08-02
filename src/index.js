2e86092a5aa1a3cbf57694defafc8427cdbcec8c
ericbsantana <bitencourteric@hotmail.com>
2021-08-02 18:43:22 -0300

    change axios baseurl

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom";
 import "./css/index.css";
 import App from "./App";
 
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+console.log(process.env.REACT_APP_API_URL);
 ReactDOM.render(
   <React.StrictMode>
     <App />
